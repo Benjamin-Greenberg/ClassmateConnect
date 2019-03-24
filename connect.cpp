859ea0aff74f277cda93f6d4c9a5c1c2320c7000
Daniel N Troutman <dtroutm1@hydra6.eecs.utk.edu>
2019-03-24 19:13:38 -0400

    added connect.cpp

---
+++ connect.cpp
@@ -0,0 +1,14 @@
+/**************************************************************
+ * Classmate Connect - Prototype for final project in CS302
+ * Written By Daniel Troutman and Benjamin Greenberg
+ * Description: Finds students who share classes together
+**************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	return 0;
+}
